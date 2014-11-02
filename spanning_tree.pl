d0536c1298b7210f12cc00fe78de4de0587f360a
amit-mittal <amit.mittal1993@gmail.com>
2014-11-03 01:12:59 +0530

    if graph connected...done

--- spanning_tree.pl
+++ spanning_tree.pl
@@ -17,28 +17,29 @@ cycle(Start, Curr, Edges):-
 %% is present in the cycle or not
 cycle_in_graph([Start/Next | Edges]):-
 					(
-					cycle(Start, Next, Edges),
-					!
+					cycle(Start, Next, Edges), !
 					);
 					cycle_in_graph(Edges).
 					
 %% Checks if path exists from X to Y					
 path(X, Y, Edges):-
-					(
-					member(X/Y, Edges);
-					member(Y/X, Edges)
-					),
-					!.
+					member(X/Y, Edges), !.
 path(X, Y, Edges):-
-					(
+					member(Y/X, Edges), !.
+path(X, Y, Edges):-
 					member(X/Next, Edges),
 					delete(Edges, X/Next, NewEdges),
-					path(Next, Y, NewEdges),
-					!
-					);
-					(
+					path(Next, Y, NewEdges), !.
+path(X, Y, Edges):-					
 					member(Next/X, Edges),
 					delete(Edges, Next/X, NewEdges),
-					path(Next, Y, NewEdges)
-					).
+					path(Next, Y, NewEdges), !.
 					
+%% Checks of graph is connected or not
+%% TODO bug not checking all possible vertices pair
+connected([], _).
+connected([_], _):- !.
+connected([Vertex, OtherVertex | Rest], Edges):-
+					path(Vertex, OtherVertex, Edges),
+					connected([Vertex | Rest], Edges),
+					connected([OtherVertex | Rest], Edges), !.
